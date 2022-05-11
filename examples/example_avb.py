b6619a355bf49841c77fc019b76cdaf9a331edcb
Martin Craig <martin.craig@nottingham.ac.uk>
2022-05-11 10:21:33 +0100

    Use spatial priors and match avb/svb options

--- examples/example_avb.py
+++ examples/example_avb.py
@@ -6,18 +6,28 @@ import sys
 import matplotlib.pyplot as plt
 import nibabel as nib
 
-from vaby_avb import run
+from vaby.main import run
 
 model = "aslrest"
 outdir = "example_avb_out"
 
 options = {
+    "data" : "asldata_diff.nii.gz",
+    "mask" : "asldata_mask.nii.gz",
+    "model_name" : model,
+    "output" : outdir,
+    "method" : "avb",
     "tau" : 1.8,
     "casl" : True,
     "plds" : [0.25, 0.5, 0.75, 1.0, 1.25, 1.5],
     "repeats" : [8],
     "slicedt" : 0.0452,
     "inferart" : True,
+    "param_overrides" : {
+        "ftiss" : {
+            "prior_type" : "M",
+        }
+    },
     "save_mean" : True,
     "save_noise" : True,
     #"save_param_history" : True,
@@ -28,14 +38,15 @@ options = {
     "save_log" : True,
     "save_input_data" : True,
     "save_var" : True,
+    "max_iterations" : 50,
     "log_stream" : sys.stdout,
 }
 
 # Run fabber as a comparison if desired
 #import os
-#os.system("fabber_asl --model=aslrest --method=vb --noise=white --data=asldata_diff --mask=asldata_mask --max-iterations=20 --incart --inferart --tau=1.8 --casl --batsd=1.0 --bat=1.3 --ti1=2.05 --ti2=2.3 --ti3=2.55 --ti4=2.8 --ti5=3.05 --ti6=3.3 --slicedt=0.0454 --inctiss --infertiss --repeats=8 --output=asl_example_fabber_out --incbat --inferbat --overwrite --save-model-fit")
+#os.system("fabber_asl --model=aslrest --method=spatialvb --param-spatial-priors=MN+ --noise=white --data=asldata_diff --mask=asldata_mask --max-iterations=50 --incart --inferart --tau=1.8 --casl --batsd=1.0 --bat=1.3 --ti1=2.05 --ti2=2.3 --ti3=2.55 --ti4=2.8 --ti5=3.05 --ti6=3.3 --slicedt=0.0454 --inctiss --infertiss --repeats=8 --output=asl_example_fabber_out --incbat --inferbat --overwrite --save-model-fit")
 
-runtime, avb = run("asldata_diff.nii.gz", model, outdir, mask="asldata_mask.nii.gz", **options)
+runtime, avb = run(**options)
 
 # Display a single slice (z=10)
 ftiss_img = nib.load("%s/mean_ftiss.nii.gz" % outdir).get_fdata()
--- examples/example_svb.py
+++ examples/example_svb.py
@@ -6,24 +6,37 @@ import sys
 import matplotlib.pyplot as plt
 import nibabel as nib
 
-from vaby_svb import run
+from vaby.main import run
 
 model = "aslrest"
-outdir = "example_svb_out"
+outdir = "example_avb_out"
 
 options = {
+    "data" : "asldata_diff.nii.gz",
+    "mask" : "asldata_mask.nii.gz",
+    "model_name" : model,
+    "output" : outdir,
+    "method" : "svb",
     "tau" : 1.8,
     "casl" : True,
     "plds" : [0.25, 0.5, 0.75, 1.0, 1.25, 1.5],
     "repeats" : [8],
     "slicedt" : 0.0452,
     "inferart" : True,
+    "param_overrides" : {
+        "ftiss" : {
+            "prior_type" : "M",
+        }
+    },
+    "epochs" : 300,
+    "learning_rate" : 0.1,
+    "batch_size" : 6,
     "save_mean" : True,
     "save_noise" : True,
     #"save_param_history" : True,
     #"save_free_energy_history" : True,
     "save_runtime" : True,
-    "save_free_energy" : True,
+    #"save_free_energy" : True,
     "save_model_fit" : True,
     "save_log" : True,
     "save_input_data" : True,
@@ -33,9 +46,9 @@ options = {
 
 # Run fabber as a comparison if desired
 #import os
-#os.system("fabber_asl --model=aslrest --method=vb --noise=white --data=asldata_diff --mask=asldata_mask --max-iterations=20 --incart --inferart --tau=1.8 --casl --batsd=1.0 --bat=1.3 --ti1=2.05 --ti2=2.3 --ti3=2.55 --ti4=2.8 --ti5=3.05 --ti6=3.3 --slicedt=0.0454 --inctiss --infertiss --repeats=8 --output=asl_example_fabber_out --incbat --inferbat --overwrite --save-model-fit")
+#os.system("fabber_asl --model=aslrest --method=spatialvb --param-spatial-priors=MN+ --noise=white --data=asldata_diff --mask=asldata_mask --max-iterations=50 --incart --inferart --tau=1.8 --casl --batsd=1.0 --bat=1.3 --ti1=2.05 --ti2=2.3 --ti3=2.55 --ti4=2.8 --ti5=3.05 --ti6=3.3 --slicedt=0.0454 --inctiss --infertiss --repeats=8 --output=asl_example_fabber_out --incbat --inferbat --overwrite --save-model-fit")
 
-runtime, svb = run("asldata_diff.nii.gz", model, outdir, mask="asldata_mask.nii.gz", **options)
+runtime, avb = run(**options)
 
 # Display a single slice (z=10)
 ftiss_img = nib.load("%s/mean_ftiss.nii.gz" % outdir).get_fdata()
